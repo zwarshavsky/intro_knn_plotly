c27c8240a51ab5cb020bbeff367954791a555235
Zhenya Warshavsky <zwarshavsky@gmail.com>
2019-10-07 11:23:29 -0400

    my plotly updates

--- app.py
+++ app.py
@@ -13,8 +13,28 @@ app.title='knn'
 
 ########### Set up the layout
 app.layout = html.Div(children=[
-    html.H1('This is the headline'),
-    html.Div(['Stuff will go here!']),
+    html.H1('Classification of Iris Flower'),
+    html.Div([
+        html.H6('Sepal Length'),
+        dcc.Slider(
+            id = 'slide-1',
+            min=1,
+            max=8,
+            step=0.1,
+            marks={i:str(i) for i in range(1,9)},
+            value=5, 
+        
+        ),
+        html.H6('Petal Length'),
+        
+        html.H6('# of Neighbors'),
+        
+        html.H6(id='output-message',children='output will go here'),
+        
+        
+             
+             
+             ]),
     html.Br(),
     html.A('Code on Github', href='https://github.com/austinlasseter/knn_iris_plotly'),
 ])
